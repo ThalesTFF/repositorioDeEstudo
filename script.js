cd702fa4e19a88589b3a879cbf367aed845803e8
Thales Fernandes de Faria <tffaria25@gmail.com>
2025-06-18 18:50:13 -0300

    Editing script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 
 const h1 = document.createElement('h1');
-h1.textContent = "Hello World";
+h1.textContent = "Repositorio do Github Desktop";
 
 container.append(h1);
